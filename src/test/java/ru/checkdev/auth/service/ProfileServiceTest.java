f745acf5166214b8cfd11a8fd1e30315df659e1c
Anton Serdyuchenko <anton415@gmail.com>
2026-04-18 10:48:01 +0300

    4. Docker сервисы [#504781]

--- src/test/java/ru/checkdev/auth/service/ProfileServiceTest.java
+++ src/test/java/ru/checkdev/auth/service/ProfileServiceTest.java
@@ -1,5 +1,6 @@
 package ru.checkdev.auth.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
